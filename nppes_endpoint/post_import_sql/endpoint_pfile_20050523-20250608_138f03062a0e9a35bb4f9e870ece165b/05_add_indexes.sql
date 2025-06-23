f504f2b06484f852b30f706b2c347220f1421b8c
Fred Trotter <fred.trotter@gmail.com>
2025-06-23 02:14:33 -0400

    works.

--- nppes_endpoint/post_import_sql/endpoint_pfile_20050523-20250608_138f03062a0e9a35bb4f9e870ece165b/05_add_indexes.sql
+++ nppes_endpoint/post_import_sql/endpoint_pfile_20050523-20250608_138f03062a0e9a35bb4f9e870ece165b/05_add_indexes.sql
@@ -2,7 +2,6 @@
 CREATE INDEX endpoint_file_NPI_index
     on REPLACE_ME_DATABASE_NAME.REPLACE_ME_TABLE_NAME (npi);
 
-ALTER TABLE REPLACE_ME_DATABASE_NAME.REPLACE_ME_TABLE_NAME
-    ADD CONSTRAINT endpoint_file_pk
-        PRIMARY KEY (endpoint_type, endpoint_type_description, npi, "endpoint");
+CREATE INDEX endpoint_file_NPI_endpoint
+    on REPLACE_ME_DATABASE_NAME.REPLACE_ME_TABLE_NAME (npi, "endpoint");
 
