20d13d8260848e03cad58a366d06db48cdc21521
Fred Trotter <fred.trotter@gmail.com>
2025-06-23 01:10:56 -0400

    make second nppes directory for endpoints

--- go_nppes_endpoint.sh
+++ go_nppes_endpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-csviper extract-metadata --from_csv=/Users/ftrotter/Downloads/NPPES_Data_Dissemination_June_2025_V2/npidata_pfile_20050523-20250608.csv --output_dir=./nppes/ --overwrite_previous
-csviper build-sql --from_metadata_json=./nppes/npidata_pfile_20050523-20250608.metadata.json --output_dir=./nppes/ --overwrite_previous
-csviper build-import-script --from_resource_dir=./nppes/ --output_dir=./nppes/ --overwrite_previous
-python ./nppes/go.postgresql.py --csv_file=/Users/ftrotter/Downloads/NPPES_Data_Dissemination_June_2025_V2/npidata_pfile_20050523-20250608.csv --db_schema_name=nppes_raw --table_name=main_file
+csviper extract-metadata --from_csv=/Users/ftrotter/Downloads/NPPES_Data_Dissemination_June_2025_V2/endpoint_pfile_20050523-20250608.csv --output_dir=./nppes_endpoint/ --overwrite_previous
+csviper build-sql --from_metadata_json=./nppes_endpoint/endpoint_pfile_20050523-20250608.metadata.json --output_dir=./nppes_endpoint/ --overwrite_previous
+csviper build-import-script --from_resource_dir=./nppes_endpoint/ --output_dir=./nppes_endpoint/ --overwrite_previous
+python ./nppes_endpoint/go.postgresql.py --csv_file=/Users/ftrotter/Downloads/NPPES_Data_Dissemination_June_2025_V2/npidata_pfile_20050523-20250608.csv --db_schema_name=nppes_raw --table_name=main_file
