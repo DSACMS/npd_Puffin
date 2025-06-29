f8fdeb9318c5904ed925bc7e4e7cfdbd2351a6cb
Fred Trotter <fred.trotter@gmail.com>
2025-06-29 00:20:15 -0400

    initial pecos

---
+++ go_pecos.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+csviper full-compile --from_csv ~/Downloads/Medicare_Fee-For-Service_Public_Provider_Enrollment/2025-Q1/PPEF_Enrollment_Extract_2025.04.01.csv --output_dir=./pecos_enrollment/ --overwrite_previous
