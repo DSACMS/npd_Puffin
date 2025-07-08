68c6a3607acee7997c4afe673a9ea35225c72af2
Fred Trotter <fred.trotter@gmail.com>
2025-07-08 13:15:05 -0400

    reassignment has two s

--- go_pecos.sh
+++ go_pecos.sh
@@ -4,5 +4,5 @@ python pecos_enrollment/go.postgresql.py --csv_file /Users/ftrotter/Downloads/Me
 
 
 csviper full-compile --from_csv ~/Downloads/Medicare_Fee-For-Service_Public_Provider_Enrollment/2025-Q1/PPEF_Reassignment_Extract_2025.04.01.csv --output_dir=./pecos_enrollment/ --overwrite_previous
-python pecos_enrollment/go.postgresql.py --csv_file /Users/ftrotter/Downloads/Medicare_Fee-For-Service_Public_Provider_Enrollment/2025-Q1/PPEF_Reassignment_Extract_2025.04.01.csv --db_schema_name=pecos_raw --table_name=pecos_reasignment
+python pecos_enrollment/go.postgresql.py --csv_file /Users/ftrotter/Downloads/Medicare_Fee-For-Service_Public_Provider_Enrollment/2025-Q1/PPEF_Reassignment_Extract_2025.04.01.csv --db_schema_name=pecos_raw --table_name=pecos_reassignment
 
---
+++ nppes_main/post_import_scripts/Step35_pecos_knows_reassignment.py
@@ -0,0 +1,7 @@
+"""
+
+The pecos_raw.pecos_e
+
+    "reasgn_bnft_enrlmt_id" 
+    "rcv_bnft_enrlmt_id"
+"""
