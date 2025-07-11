29bbe350b6b4a9675bb4f93b2c76251a90a02b08
Fred Trotter <fred.trotter@gmail.com>
2025-07-11 17:58:08 -0400

    lowercase payer

--- sql/create_table_sql/create_payer_data.sql
+++ sql/create_table_sql/create_payer_data.sql
@@ -3,63 +3,63 @@
 -- while still ultimately being compatible with the data we are required to express as low-level (i.e. the strict parts)
 -- of FHIR compliance. 
 
-CREATE TABLE ndh.PayerToInteropEndpoint (
+CREATE TABLE ndh.payer_interop_endpoint (
     id SERIAL PRIMARY KEY,
-    Payer_id int   NOT NULL,
-    InteropEndpoint_id int   NOT NULL
+    payer_id int   NOT NULL,
+    interop_endpoint_id int   NOT NULL
 );
 
-CREATE TABLE ndh.Payer (
+CREATE TABLE ndh.payer (
     -- marketplace/network-puf.IssuerID
     id SERIAL PRIMARY KEY,
     -- marketplace/plan-attributes-puf.IssuerMarketPlaceMarketingName
-    PayerName varchar   NOT NULL
+    payer_nme varchar   NOT NULL
 );
 
 -- There are a lot of atributes for plans, not sure how much we need to include
-CREATE TABLE ndh.Plan (
+CREATE TABLE ndh.plan (
     -- marketplace/plan-attributes-puf.PlanId
     id SERIAL PRIMARY KEY,
-    Payer_id int   NOT NULL,
-    MarketCoverage_id int   NOT NULL,
+    payer_id int   NOT NULL,
+    marketcoverage_id int   NOT NULL,
     -- marketplace/plan-attributes-puf.ServiceAreaId
-    ServiceArea_id int   NOT NULL,
+    service_area_id int   NOT NULL,
     -- marketplace/plan-attributes-puf.DentalOnlyPlan
-    DentalOnlyPlan boolean   NOT NULL,
+    is_dental_only_lan boolean   NOT NULL,
     -- marketplace/plan-attributes-puf.PlanMarketingName
-    PlanMarketingName varchar   NOT NULL,
+    plan_marketing_name varchar   NOT NULL,
     -- marketplace/plan-attributes-puf.HIOSProductId
-    HIOSProductID varchar   NOT NULL,
-    PlanType_id int   NOT NULL,
+    hios_product_id varchar   NOT NULL,
+    plan_type_id int   NOT NULL,
     -- marketplace/plan-attributes-puf.IsNewPlan
-    IsNewPlan boolean   NOT NULL
+    is_new_plan boolean   NOT NULL
 );
 
-CREATE TABLE ndh.PlanType (
+CREATE TABLE ndh.plan_type (
     id SERIAL PRIMARY KEY,
     -- marketplace/plan-attributes-puf.PlanType
-    PlanType varchar   NOT NULL
+    plan_type_name varchar   NOT NULL
 );
 
-CREATE TABLE ndh.MarketCoverage (
+CREATE TABLE ndh.marketcoverage (
     id SERIAL PRIMARY KEY,
     -- marketplace/plan-attributes-puf.MarketCoverage
-    MarketCoverage varchar   NOT NULL
+    marketcoverage_name varchar   NOT NULL
 );
 
-CREATE TABLE ndh.PlanNetworkToPlan (
+CREATE TABLE ndh.plan_network_plan (
     id SERIAL PRIMARY KEY,
-    Plan_id int   NOT NULL,
-    PlanNetwork_id int   NOT NULL
+    plan_id int   NOT NULL,
+    plan_network_id int   NOT NULL
 );
 
-CREATE TABLE ndh.plannetwork (
+CREATE TABLE ndh.plan_network (
     -- marketplace/network-puf.NetworkID
     id SERIAL PRIMARY KEY,
     -- marketplace/network-puf.NetworkName
-    plannetwork_name varchar(100)   NOT NULL,
+    plan_network_name varchar(100)   NOT NULL,
     -- marketplace/network-puf.NetworkURL
-    plannetwork_url varchar(500)   NOT NULL
+    plan_network_url varchar(500)   NOT NULL
 );
 
 -- TODO how does this relate to FIPS counties and the sub-counties concept from MA plans?
@@ -75,8 +75,8 @@ CREATE TABLE ndh.service_area (
 );
 
 -- PECOS Sourced initially, then UX Maintained
-CREATE TABLE ndh.plannetwork_clinical_organization (
+CREATE TABLE ndh.plan_network_clinical_organization (
     id SERIAL PRIMARY KEY,
-    plannetwork_id int   NOT NULL,
+    plan_network_id int   NOT NULL,
     clinical_organization_id int   NOT NULL
 );
