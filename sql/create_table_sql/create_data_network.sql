94510055b26fd5fb4eff71c015776350c537e538
Fred Trotter <fred.trotter@gmail.com>
2025-07-14 12:57:08 -0400

    better comments

--- sql/create_table_sql/create_data_network.sql
+++ sql/create_table_sql/create_data_network.sql
@@ -1,12 +1,9 @@
 
-
+-- elements needed to power a homepage list of data networks
 
 CREATE TABLE ndh.data_network (
-    -- marketplace/network-puf.NetworkID
     id SERIAL PRIMARY KEY,
-    -- marketplace/network-puf.NetworkName
     data_network_name varchar(100)   NOT NULL,
-    -- marketplace/network-puf.NetworkURL
     data_network_blurb TEXT DEFAULT NULL,
     data_network_url varchar(500)   DEFAULT NULL,
     data_network_logo_url varchar(500)   DEFAULT NULL,
